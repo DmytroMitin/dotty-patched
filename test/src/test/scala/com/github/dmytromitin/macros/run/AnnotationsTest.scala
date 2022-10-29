1ad8aa042799db86a584faf87366c15bb3cd2493
DmytroMitin <dmitin3@gmail.com>
2022-10-29 07:35:35 +0300

    Fix AnnotationsTest

--- test/src/test/scala/com/github/dmytromitin/macros/run/AnnotationsTest.scala
+++ test/src/test/scala/com/github/dmytromitin/macros/run/AnnotationsTest.scala
@@ -7,8 +7,8 @@ import matchers.*
 class AnnotationsTest extends AnyFlatSpec with should.Matchers:
   "AnnotationsMacro.getAnnotations" should "return annotations for CanEqual" in {
     AnnotationsMacro.getAnnotations(classOf[scala.CanEqual[?, ?]]) should be (Seq(
-      "scala.annotation.internal.SourceFile",
-      "scala.annotation.internal.Child",
-      "scala.annotation.implicitNotFound"
+      """new scala.annotation.internal.SourceFile("library/src/scala/CanEqual.scala")""",
+      "new scala.annotation.internal.Child[scala.CanEqual.derived.type]()",
+      """new scala.annotation.implicitNotFound("Values of types ${L} and ${R} cannot be compared with == or !=")"""
     ))
   }
